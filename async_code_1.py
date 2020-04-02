f69950279a4d110efe90015bcefdd2542a2ab6a9
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2020-04-02 22:26:11 +0300

    add first async def

--- async_code_1.py
+++ async_code_1.py
@@ -1,2 +1,25 @@
 from time import sleep
+from time import time
 import asyncio
+
+start = time()
+
+async def spider(name):
+    for page in range(1, 4):
+        await asyncio.sleep(1)
+        print(name, page)
+
+spiders = [
+    asyncio.ensure_future(spider('Blog_1')),
+    asyncio.ensure_future(spider('Blog_2')),
+    asyncio.ensure_future(spider('Blog_3'))
+]
+
+event_loop = asyncio.get_event_loop()
+event_loop.run_until_complete(asyncio.gather(*spiders))
+event_loop.close()
+
+
+print("{:.2F}".format(time() - start))
+
+
